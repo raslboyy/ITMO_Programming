c5084384eb2abcb88091be446a03d4a6912ebbc0
raslboyy <ruslankhakimov67@gmail.com>
2021-05-19 06:48:25 +0300

    lab2.05 fix iterator

--- lab2.05/main.cpp
+++ lab2.05/main.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
+#include <algorithm>
 #include "src/circular_buffer.h"
 
-template<class T>
-class Integers
-{
-public:
-    struct Iterator
-    {
-        using iterator_category = std::forward_iterator_tag;
-        using difference_type   = std::ptrdiff_t;
-        using value_type        = T;
-        using pointer           = T*;
-        using reference         = T&;
-
-        Iterator(pointer ptr) : m_ptr(ptr) {}
-
-        reference operator*() const { return *m_ptr; }
-        pointer operator->() { return m_ptr; }
-        Iterator& operator++() { m_ptr++; return *this; }
-        Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
-        friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
-        friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
-
-    private:
-        pointer m_ptr;
-    };
-
-    Integers(T k) : value(k) {
-        Iterator it(nullptr);
-    };
-
-    Iterator begin() { return Iterator(&m_data[0]); }
-    Iterator end()   { return Iterator(&m_data[200]); }
-
-private:
-    T m_data[200];
-    T value;
-};
+using std::cout;
 
 int main() {
 
-//    Integers<int> a(4);
-//    a.begin();
-//    a.end();
-    circular_buffer<int> a(3);
-//    a.push_back(1);
-//    a.push_back(2);
-//    a.push_back(3);
+  circular_buffer<int> a(3);
+  a.push_back(2);
+  a.push_back(1);
+  a.push_back(3);
+  a.pop_back();
+  a.pop_front();
+  a.push_back(2);
+  a.push_back(3);
+  cout << a;
+  a.push_back(4);
+  cout << a;
+  a.push_front(5);
+  cout << a;
+  a.push_front(6);
+  cout << a;
+  a.push_front(7);
+  cout << a;
+  a.pop_front();
+  cout << a;
+  a.pop_back();
+  cout << a;
 
-//    std:: cout << a;
+  circular_buffer<int> b(3);
+  b.push_front(2);
+  b.push_front(1);
+  b.push_front(3);
+  for (int i = 0; i < 3; i++)
+    cout << b[i] << ' ';
+  cout << '\n';
 
-    return 0;
+  b.set_capacity(6);
+  b.push_front(3);
+  b.push_back(2);
+  cout << b;
+  b.push_back(4);
+  cout << b;
+  b.push_back(4);
+  cout << b;
+
+  circular_buffer<int> c(15);
+  for (int i = 0; i < 10; i++)
+    c.push_back(10 - i);
+  cout << c;
+  std::sort(c.begin(), c.end());
+  cout << c;
+
+  return 0;
 }
--- lab2.05/src/circular_buffer.h
+++ lab2.05/src/circular_buffer.h
@@ -8,163 +8,195 @@
 
 template<class T>
 class circular_buffer {
-public:
-    class iterator;
+ public:
+  class iterator;
 
-    circular_buffer();
+  circular_buffer();
+  explicit circular_buffer(size_t capacity);
+  circular_buffer(const circular_buffer<T> &other);
 
-    explicit circular_buffer(size_t capacity);
+  void swap(circular_buffer &other);
+  circular_buffer<T> &operator=(const circular_buffer<T> &other);
 
-    circular_buffer(const circular_buffer<T> &other);
+  ~circular_buffer();
 
-    void swap(circular_buffer &other);
-
-    circular_buffer<T> &operator=(const circular_buffer<T> &other);
-
-    ~circular_buffer();
-
-    void push_back(const T &item) { *(end_++) = item; }
-    void push_front(const T &item) { *(--begin_) = item; }
-
-    void pop_back() { --end_; }
-    void pop_front() { ++begin_; }
-
-    iterator begin() const { return begin_; }
-    iterator end() const { return end_; }
-
-    T front() const { return array_[begin_]; }
-    T back() const { return array_[end_ % capacity_]; };
-
-    T operator[](size_t index) const { return array_[(begin_ + index) % capacity_]; };
-    T &operator[](size_t index) { return array_[(begin_ + index) % capacity_]; }
-
-    void set_capacity(size_t capacity);
-    [[nodiscard]] size_t capacity() const { return capacity_; }
-    [[nodiscard]] size_t size() const { return (end_ - begin_) % capacity_; }
-
-    template<class U>
-    friend std::ostream &operator<<(std::ostream &os, const circular_buffer<U> &cb);
-
-
-private:
-    T *array_;
-    size_t capacity_{};
-    iterator begin_{};
-    iterator end_{};
-
-    void mod(T &item) {
-        item %= capacity_;
+  void push_back(const T &item) {
+    *(begin_ + size_++) = item;
+    if (size_ == capacity_) {
+      begin_++;
+      size_--;
     }
+  }
+  void push_front(const T &item) {
+    *(--begin_) = item;
+    size_++;
+    if (size_ == capacity_)
+      size_--;
+  }
+
+  void pop_back() {
+    size_ = std::max(0ul, size_ - 1);
+  }
+  void pop_front() {
+    ++begin_;
+    size_ = std::max(0ul, size_ - 1);
+  }
+
+  iterator begin() const { return begin_; }
+  iterator end() const { return begin_ + size_; }
+  T front() const { return *begin(); }
+  T back() const { return *end(); };
+
+  T operator[](size_t index) const { return *(begin_ + index); };
+  T &operator[](size_t index) { return *(begin_ + index); }
+
+  void set_capacity(size_t capacity);
+
+  [[nodiscard]] size_t capacity() const { return capacity_; }
+  [[nodiscard]] size_t size() const { return size_; }
+
+  template<class U>
+  friend std::ostream &operator<<(std::ostream &os, const circular_buffer<U> &cb);
+
+ private:
+  T *array_;
+  size_t capacity_;
+  size_t size_;
+  iterator begin_;
 };
 
 template<class T>
 std::ostream &operator<<(std::ostream &os, const circular_buffer<T> &cb) {
-    for (auto i : cb)
-        os << i << ' ';
-    os << '\n';
-    return os;
+  for (auto i : cb)
+    os << i << ' ';
+  os << '\n';
+  return os;
 }
 
 template<class T>
 circular_buffer<T>::circular_buffer(size_t capacity) :
-        array_(new T[capacity + 1]),
-        capacity_(capacity + 1),
-        begin_(array_),
-        end_(array_) {
-            iterator a(nullptr);
-            typename iterator::difference_type iterator::size = capacity + 1;
-        }
+    array_(new T[capacity + 1]),
+    capacity_(capacity + 1),
+    size_(0) {
+  begin_ = iterator(array_, &array_, capacity_);
+}
 
 template<class T>
 circular_buffer<T>::circular_buffer() :
-        circular_buffer(1) {}
+    circular_buffer(0) {}
 
 template<class T>
 circular_buffer<T>::circular_buffer(const circular_buffer<T> &other) :
-        array_(new T[other.capacity_]),
-        capacity_(other.capacity_),
-        begin_(other.begin_),
-        end_(other.end_) {
-    for (size_t i = 0; i != other.capacity_; i++)
-        array_[i] = other.array_[i];
+    array_(new T[other.capacity_]),
+    capacity_(other.capacity_),
+    begin_(other.begin_),
+    size_(other.size_) {
+  for (size_t i = 0; i != other.capacity_; i++)
+    array_[i] = other.array_[i];
 }
 
 template<class T>
 void circular_buffer<T>::swap(circular_buffer &other) {
-    std::swap(this->array_, other.array_);
-    std::swap(this->begin_, other.begin_);
-    std::swap(this->end_, other.end_);
-    std::swap(this->capacity_, this->capacity_);
+  std::swap(this->array_, other.array_);
+  std::swap(this->begin_, other.begin_);
+  std::swap(this->end_, other.end_);
+  std::swap(this->capacity_, this->capacity_);
 }
 
 template<class T>
 circular_buffer<T> &circular_buffer<T>::operator=(const circular_buffer<T> &other) {
-    circular_buffer(other).swap(*this);
-    return *this;
+  circular_buffer(other).swap(*this);
+  return *this;
 }
 
 template<class T>
 circular_buffer<T>::~circular_buffer() {
-    delete[] array_;
-    array_ = nullptr;
+  delete[] array_;
+  array_ = nullptr;
 }
 
 template<class T>
 void circular_buffer<T>::set_capacity(size_t capacity) {
-    T *array = new T[capacity + 1];
-    for (size_t i = 0; i != capacity_; i++)
-        array[i] = array_[i];
-    delete[] array_;
-    array_ = array;
+  T *array = new T[capacity + 1];
+  for (size_t i = 0; i != capacity_; i++)
+    array[i] = array_[i];
+  capacity_ = capacity + 1;
+  begin_ = iterator(array + (static_cast<T *>(begin_) - array_), &array_, capacity_);
+  delete[] array_;
+  array_ = array;
 }
 
 template<class T>
 class circular_buffer<T>::iterator {
-public:
-    using iterator_category = std::random_access_iterator_tag;
-    using difference_type = std::ptrdiff_t;
-    using value_type = T;
-    using pointer = T *;
-    using reference = T &;
+ public:
+  using iterator_category = std::random_access_iterator_tag;
+  using difference_type = std::ptrdiff_t;
+  using value_type = T;
+  using pointer = T *;
+  using reference = T &;
 
-    explicit iterator(pointer ptr) : ptr_(ptr) {}
+  iterator() : ptr_(nullptr), begin_(nullptr), size_(0) {}
+  explicit iterator(pointer ptr, pointer *begin, difference_type size) :
+      ptr_(ptr),
+      begin_(begin),
+      size_(size) {}
 
-    reference operator*() const { return *ptr_; }
-    pointer operator->() { return ptr_; }
+  reference operator*() const { return *ptr_; }
+  pointer operator->() { return ptr_; }
+  explicit operator T *() const { return ptr_; }
 
-    iterator &operator++() { forth(++ptr_); return *this; }
-    iterator operator++(int) { iterator tmp = *this; ++(*this); return tmp; }
-    iterator &operator--() { back(--ptr_); return *this; }
-    iterator operator--(int) { iterator tmp = *this; --(*this); return tmp; }
+  iterator &operator++() {
+    ++ptr_;
+    if (ptr_ == *begin_ + size_)
+      ptr_ = *begin_;
+    return *this;
+  }
+  iterator operator++(int) {
+    iterator tmp = *this;
+    ++(*this);
+    return tmp;
+  }
 
-//        iterator &operator+=(difference_type n) { forth(ptr_ += n, n); return *this; }
-//        iterator &operator-=(difference_type n) { back(ptr_ -= n, n); return *this; }
+  iterator &operator--() {
+    --ptr_;
+    if (ptr_ == *begin_ - 1)
+      ptr_ = *begin_ + size_ - 1;
+    return *this;
+  }
+  iterator operator--(int) {
+    iterator tmp = *this;
+    --(*this);
+    return tmp;
+  }
 
-    friend bool operator==(const iterator &a, const iterator &b) { return a.ptr_ == b.ptr_; }
-    friend bool operator!=(const iterator &a, const iterator &b) { return a.ptr_ != b.ptr_; }
+  iterator &operator+=(difference_type n) {
+    ptr_ = *begin_ + (ptr_ - *begin_ + n) % size_;
+    return *this;
+  }
+  iterator &operator-=(difference_type n) {
+    ptr_ = *begin_ + (ptr_ - *begin_ - n % size_ + size_) % size_;
+    return *this;
+  }
 
-//        friend iterator operator+(iterator a, difference_type n) { return a += n; }
-//        friend iterator operator+(difference_type n, iterator a) { return a += n; }
-//        friend iterator operator-(iterator a, difference_type n) { return a -= n; }
-//        friend iterator operator-(iterator a, iterator b) { return (a.ptr_ - b.ptr_) % size; }
+  reference operator[](difference_type index) { return *(ptr_ + index); }
+  value_type operator[](difference_type index) const { return *(ptr_ + index); }
 
-    static pointer begin;
-    static difference_type size;
-private:
-    pointer ptr_;
+  friend bool operator==(const iterator &a, const iterator &b) { return a.ptr_ == b.ptr_; }
+  friend bool operator!=(const iterator &a, const iterator &b) { return a.ptr_ != b.ptr_; }
+  friend bool operator<(const iterator &a, const iterator &b) { return a.ptr_ < b.ptr_; }
+  friend bool operator>(const iterator &a, const iterator &b) { return b < a; }
+  friend bool operator<=(const iterator &a, const iterator &b) { return a.ptr_ <= b.ptr_; }
+  friend bool operator>=(const iterator &a, const iterator &b) { return b <= a; }
 
-    void forth(pointer &ptr) {
-        forth(ptr, 1);
-    }
-    void back(pointer &ptr) {
-        back(ptr, 1);
-    }
-    void forth(pointer &ptr, difference_type n) {
-        ptr = begin + (ptr - begin + n) % (size);
-    }
-    void back(pointer &ptr, difference_type n) {
-        ptr = begin + (ptr - begin - n) % (size);
-    }
+  friend iterator operator+(iterator a, difference_type n) { return a += n; }
+  friend iterator operator+(difference_type n, iterator a) { return a += n; }
+  friend iterator operator-(iterator a, difference_type n) { return a -= n; }
+  friend difference_type operator-(iterator a, iterator b) { return a.ptr_ - b.ptr_; }
+
+ private:
+  pointer *begin_;
+  difference_type size_;
+  pointer ptr_;
 };
 
 #endif //LAB2_05_CIRCULAR_BUFFER_H
